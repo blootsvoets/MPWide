19a9aa37b5ce02573a1873137a354ca9dee9b883
Joris Borgdorff <jorisb@int1.cartesius.surfsara.nl>
2013-07-31 15:58:50 +0200

    Updated PingPong

--- PingPong.cpp
+++ PingPong.cpp
@@ -4,67 +4,67 @@
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
+#include <sys/time.h>
 
 using namespace std;
 
 //#define ACT_AS_SERVER
 
+#ifdef ACT_AS_SERVER
+static inline long GetLongTime()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec*1000000l+tv.tv_usec;
+}
+#endif
+
 #include "MPWide.h"
 
 int main(int argc, char** argv)
 {
-  int num_channels = 1;
-  int msgsize = 8;
-
-  if(argc<=2) {
-    printf("usage: ./MPWTest <ip address of other endpoint> <repeat> [<channels> [<data size [kb]> [<pacing [MB/s]> [<tcpwin [bytes>]]]]]\n All parameters after the first are optional.\n");
+  if(argc!=5) {
+    printf("usage: ./MPWPingPong <ip address of other endpoint> <repeat> <channels> <data size [kb]>\n");
     return 1;
   }
 
   /* Initialize */
   string host = string(argv[1]);
-  int repeat = atoi(argv[2]);
+  const int repeat       = atoi(argv[2]);
+  const int num_channels = atoi(argv[3]);
+  const int msgsize      = atoi(argv[4]);
 
-  if(argc>3)
-    num_channels = atoi(argv[3]);
+  const int path_id = MPW_CreatePathWithoutConnect(host, 20003, num_channels);
+  if (path_id == -1)
+	exit(EXIT_FAILURE);
 
-  if(argc>4)
-    msgsize = atoi(argv[4]);
-
-  if(argc>5)
-    MPW_setPacingRate(atoi(argv[5])*1024*1024);
-  
-  int path_id = MPW_CreatePathWithoutConnect(host, 20506, num_channels);
 #ifdef ACT_AS_SERVER
-  MPW_ConnectPath(path_id, true); ///path version
+  MPW_ConnectPath(path_id, true);
 #else
-  MPW_ConnectPath(path_id, false); ///path version
+  MPW_ConnectPath(path_id, false);
 #endif
 
-  if (argc > 6)
-    MPW_setPathWin(path_id, atoi(argv[6]));
+  const size_t len = msgsize*1024; 
+  char* msg = new char[len]();
 
-  {
-    size_t len = msgsize*1024; 
-    char msg2 = 0;
-    char* msg = new char[len]();
-  
-    /* test loop */
-    for(int i=0; i<repeat; i++) {
+  /* test loop */
+  for(int i=0; i<repeat; i++) {
 #ifdef ACT_AS_SERVER
-      MPW_SendRecv(msg,len,&msg2,1,path_id); ///path version
-      MPW_SendRecv(&msg2,1,msg,len,path_id); ///path version
+    long usec = GetLongTime();
+    MPW_SendRecv(msg,len,NULL,0,path_id);
+    MPW_SendRecv(NULL,0,msg,len,path_id);
+    usec = GetLongTime() - usec;
+    printf("%ld\n", usec);
 #else
-      MPW_SendRecv(&msg2,1,msg,len,path_id); ///path version
-      MPW_SendRecv(msg,len,&msg2,1,path_id); ///path version
+    MPW_SendRecv(NULL,0,msg,len,path_id);
+    MPW_SendRecv(msg,len,NULL,0,path_id);
 #endif
-      cout << "End of iteration " << i << "." << endl;
-    }
-
-    delete [] msg;
   }
+
+  delete [] msg;
 
   MPW_Finalize();
 
   return 0;
 }
+
